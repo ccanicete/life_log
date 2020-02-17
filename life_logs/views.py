cdf111f0f3e59a8b2b34ce26d713310a516a9911
charlie <ccanicete@gmail.com>
2020-02-17 12:08:20 +0800

    used the get_object_or_404() for better security.

--- life_logs/views.py
+++ life_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -22,7 +22,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # make sure the topic belongs to the current user.
     if topic.owner != request.user:
         raise Http404
@@ -54,7 +54,7 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
 
     if request.method != 'POST':
         # no data submitted; create a blank form.
@@ -76,7 +76,7 @@ def new_entry(request, topic_id):
 @login_required
 def edit_entry(request, entry_id):
     """edit an existing entry."""
-    entry = Entry.objects.get(id=entry_id)
+    entry = get_object_or_404(Entry, id=entry_id)
     topic = entry.topic
     if topic.owner != request.user:
         raise Http404
